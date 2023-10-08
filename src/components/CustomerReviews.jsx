930a35955a7099e7649f89cff64a5950f10d2477
victor Macharia <Techvictor653@gmail.com>
2023-10-08 10:48:48 +0300

    Completed website.

--- src/components/CustomerReviews.jsx
+++ src/components/CustomerReviews.jsx
@@ -1,19 +1,29 @@
-// ///////////////////////////
+import { Reviews } from "../constants"
+import ReviewCard from "./ReviewCard";
 
 const CustomerReviews = () => {
   return (
-    <section>
-      <div className="max-container ">
-        <h3 className="font-palanquin text-center text-4xl font-bold">
-          what our <span className="text-coral-red">Customers</span> say?
-        </h3>
-        <p className="info-text m-auto mt-4 max-w-lg">
-          Hear genuine stories from our satisfied customers about their
-          exeptional experiences with us
-        </p>
-        <div className="mt-24 flex flex-1 justify-evenly">
-          
-        </div>
+    <section className='max-container'>
+      <h3 className='font-palanquin text-center text-4xl font-bold'>
+        What Our
+        <span className='text-coral-red'> Customers </span>
+        Say?
+      </h3>
+      <p className='m-auto mt-4 max-w-lg  text-center info-text'>
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+        {Reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
       </div>
     </section>
   );
